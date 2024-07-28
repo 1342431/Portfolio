23ec3cc7929b46daf77944174df2fedd22f0d742
Ashish Kongati <ashishkongati@ashishs-mbp.home>
2024-07-28 09:00:32 -0400

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
             <img
               className="logo"
               type="image/svg+xml"
-              src="/assets/itagnokTitle.png"
+              src="./assets/itagnokTitle.png"
               width="90"
               height="50"
             />
